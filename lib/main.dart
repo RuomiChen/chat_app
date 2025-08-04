4907eb78bf40896377953f5be6f689aab66f90f3
zihao.chen <zihao.chen@xilancloud.com>
2025-08-04 10:37:51 +0800

    feat:init splash_view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/routes/app_pages.dart';
+import 'package:chat_app/theme/app_theme.dart';
+import 'package:chat_app/views/auth/splash_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
@@ -9,12 +17,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return GetMaterialApp(
+      title: 'Chat App',
+      theme: AppTheme.lightTheme,
+      themeMode: ThemeMode.light,
+      initialRoute: AppPages.initial,
+      getPages: AppPages.routes,
+      debugShowCheckedModeBanner: false,
+
+      home: SplashView(),
     );
   }
 }
---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,33 @@
+import 'package:chat_app/routes/app_routes.dart';
+import 'package:chat_app/views/auth/splash_view.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
+
+class AppPages {
+  static const initial = AppRoutes.splash;
+
+  static final routes = [
+    GetPage(name: AppRoutes.splash, page: () => const SplashView()),
+    // GetPage(name: AppRoutes.login, page: ()=>const LoginView()),
+    // GetPage(name: AppRoutes.register, page: ()=>const RegisterView()),
+    // GetPage(name: AppRoutes.forgotPassword, page: ()=>const ForgotPasswordView()),
+    // GetPage(name: AppRoutes.changePassword, page: ()=>const ChangePasswordView()),
+    // GetPage(name: AppRoutes.home, page: ()=>const HomeView()),
+    // GetPage(name: AppRoutes.main, page: ()=>const MainView()),
+    // GetPage(name: AppRoutes.profile, page: ()=>const ProfileView()),
+    // GetPage(name: AppRoutes.chat, page: ()=>const ChatView()),
+    // GetPage(name: AppRoutes.usersList, page: ()=>const UsersListView()),
+    // GetPage(name: AppRoutes.friends, page: ()=>const FriendsView()),
+    // GetPage(name: AppRoutes.friendRequests,
+    //  page:()=>const FriendRequestsView(),
+    // binding: BindingsBuilder((){
+    //   Get.put(FriendRequestsController());
+    //   })
+    //   ),
+    // GetPage(name: AppRoutes.notifications, page:()=>const NotificationsView(),
+    // binding: BindingsBuilder((){
+    //   Get.put(NotificationsController());
+    //   })
+    // ),
+  ];
+}
